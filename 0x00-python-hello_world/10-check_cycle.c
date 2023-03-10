2cdc17b5080a6fb4780d923cbeed9003174d6813
artb433 <tahiruabdulrahmanbaaba3@gmail.com>
2023-03-10 22:11:19 +0000

    mtcheeeeeeww

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - check if a linked list has cycles
+ *
+ * @list: head of linked list
+ *
+ * Return: 0 if there are no cycles; otherwise 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp, *iter;
+
+	if (list == NULL)
+		return (0);
+
+	temp = list;
+	iter = list;
+
+	while (iter->next != NULL)
+	{
+		iter = iter->next;
+
+		if (iter == temp)
+			return (1);
+	}
+
+	return (0);
+}
